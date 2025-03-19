772d7dcc70a19988fa9022fabbd87143420b17ab
Aashlesha-Chitte <114223598+Aashlesha-Chitte@users.noreply.github.com>
2025-03-19 14:40:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Aashlesha_Chitte_Portfolio",
+  // base: "/Aashlesha_Chitte_Portfolio",
 });
